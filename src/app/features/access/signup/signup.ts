3cce4d605eec771bb655ee587fc6a33d3e432e78
Nestor <mateoprats.nestor@gmail.com>
2025-08-04 12:05:43 +0200

    Feat: Logic and html to signup

--- src/app/features/access/signup/signup.ts
+++ src/app/features/access/signup/signup.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { materialModules } from '../../../material';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormGroup, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [materialModules, ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss'
 })
 export class Signup {
+  /**
+   * signupForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+  */
+ signupForm!: FormGroup;
+ hidePassword = true;
+
+ constructor( private fb: FormBuilder,
+  private snackBar: MatSnackBar,
+  private authService: AuthService,
+  private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      confirmPassword: [null, [Validators.required]],
+    })
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit(): void{
+    const password = this.signupForm.get('password')?.value;
+    const confirmPassword = this.signupForm.get('confirmPassword')?.value;
+
+    if(password !== confirmPassword){
+      this.snackBar.open('Passwords do not match.', 'Close', { duration: 5000, panelClass: 'error-snackbar' })
+      return;
+    }
+
+    this.authService.register(this.signupForm.value).subscribe(
+      (response) => {
+        this.snackBar.open('Sign up successful!', 'Close' { duration: 5000 });
+        this.router.navigateByUrl("/login");
+      },
+      (error) => {
+        this.snackBar.open('Sign up failed. Please try again.', 'Close', { duration: 5000, panelClass: 'error-snackbar' })
+      }
+    )
+  }
 
 }

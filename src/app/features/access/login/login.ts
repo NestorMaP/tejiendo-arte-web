20da4685aea9f355aeed84db99a2df9f3f2503f3
Nestor <mateoprats.nestor@gmail.com>
2025-08-05 18:14:22 +0200

    feat: imports to login.ts

--- src/app/features/access/login/login.ts
+++ src/app/features/access/login/login.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { MaterialModules } from '../../../material';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { AuthService } from '../../../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.html',
-  styleUrls: ['./login.scss']
+  styleUrls: ['./login.scss'],
+  imports: [MaterialModules,
+    ReactiveFormsModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
+  ],
 })
 export class Login {
 

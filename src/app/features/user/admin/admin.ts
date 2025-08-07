2f47e6488403b8bd7ae3dfae71c5995b573f117c
Nestor <mateoprats.nestor@gmail.com>
2025-08-07 17:02:13 +0200

    Feat: Add imports to admin.ts

--- src/app/features/user/admin/admin.ts
+++ src/app/features/user/admin/admin.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './admin.html',
   styleUrl: './admin.scss'
 })

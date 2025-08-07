a6dba65f0487d6c0720e5d0f5ac884304f088555
Nestor <mateoprats.nestor@gmail.com>
2025-08-07 17:43:35 +0200

    Feat: Post-Category ts

--- src/app/features/user/admin/components/post-category/post-category.ts
+++ src/app/features/user/admin/components/post-category/post-category.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatError, MatFormField, MatLabel } from '@angular/material/input';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { AdminService } from '../../service/admin';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-post-category',
@@ -22,5 +24,41 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './post-category.scss'
 })
 export class PostCategory {
+
+  categoryForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private adminService: AdminService,
+  ) { }
+
+  ngOnInit(): void {
+    this.categoryForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+    })
+  }
+
+  addCategory(): void {
+    if(this.categoryForm.valid) {
+      this.adminService.addCategory(this.categoryForm.value).subscribe((result) => {
+        if (result.id != null) {
+          this.snackBar.open('Category Posted Successfully!', 'Close', {
+            duration: 5000
+          });
+          this.router.navigateByUrl('/admin/dashboard');
+        } else {
+          this.snackBar.open(result.message, 'Close', {
+            duration: 5000,
+            panelClass: 'error-snackbar'
+          });
+        }
+      })
+    } else {
+      this.categoryForm.markAllAsTouched();
+    }
+  }
 
 }

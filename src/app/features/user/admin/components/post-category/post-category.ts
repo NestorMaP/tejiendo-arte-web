8ca9d47217cddeecad8a163e39abee6c3ba2754b
Nestor <mateoprats.nestor@gmail.com>
2025-08-07 17:34:24 +0200

    Feat: Post-Category html

--- src/app/features/user/admin/components/post-category/post-category.ts
+++ src/app/features/user/admin/components/post-category/post-category.ts
@@ -2,12 +2,16 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatError, MatFormField, MatLabel } from '@angular/material/input';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-post-category',
   standalone:true,
   imports: [
+    MatFormField,
+    MatLabel,
+    MatError,
     CommonModule,
     HttpClientModule,
     ReactiveFormsModule,

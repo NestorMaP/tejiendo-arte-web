fa3ee2be0777505fda405c2b2eb52389d9ee2bfe
Nestor <mateoprats.nestor@gmail.com>
2025-08-07 11:58:22 +0200

    feat: new route for admin and customer dashboards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,41 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/home-example/home').then(m => m.HomeComponent)
+    loadComponent: () => import('./features/home-example/home')
+    .then(m => m.HomeComponent)
   },
   {
     path: 'login',
-    loadComponent: () => import('./features/access/login/login').then(m => m.Login)
+    loadComponent: () => import('./features/access/login/login')
+    .then(m => m.Login)
   },
   {
     path: 'signup',
-    loadComponent: () => import('./features/access/signup/signup').then(m => m.Signup)
+    loadComponent: () => import('./features/access/signup/signup')
+    .then(m => m.Signup)
   },
   {
     path: 'customer',
-    loadComponent: () => import('./features/user/customer/customer').then(m => m.Customer)
+    loadComponent: () => import('./features/user/customer/customer')
+    .then(m => m.Customer),
+    children: [
+      {
+      path: 'dashboard',
+      loadComponent: () => import('./features/user/customer/components/dashboard/dashboard')
+      .then(m => m.Dashboard),
+      },
+    ]
   },
   {
     path: 'admin',
-    loadComponent: () => import('./features/user/admin/admin').then(m => m.Admin)
+    loadComponent: () => import('./features/user/admin/admin')
+    .then(m => m.Admin),
+    children: [
+      {
+      path: 'dashboard',
+      loadComponent: () => import('./features/user/admin/components/dashboard/dashboard')
+      .then(m => m.Dashboard),
+      },
+    ]
   }
 ];

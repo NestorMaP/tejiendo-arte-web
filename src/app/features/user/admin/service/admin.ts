b37cea56b10f8fae43f66245c55acf43e4d3e4a9
Nestor <mateoprats.nestor@gmail.com>
2025-08-07 16:58:13 +0200

    Feat: Initial logic for AdminService

--- src/app/features/user/admin/service/admin.ts
+++ src/app/features/user/admin/service/admin.ts
@@ -1,8 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+  const BASIC_URL = "http://localhost:8080/"
 
 @Injectable({
   providedIn: 'root'
 })
-export class Admin {
+export class AdminService {
   
+  constructor (private http: HttpClient) {}
+
 }

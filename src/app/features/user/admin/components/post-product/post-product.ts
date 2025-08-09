c1031ca5ff4c80ad9bbfb01886b2b0d7a1c19425
Nestor <mateoprats.nestor@gmail.com>
2025-08-09 16:15:21 +0200

    Feat: Create post-product component

---
+++ src/app/features/user/admin/components/post-product/post-product.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-product',
+  imports: [],
+  templateUrl: './post-product.html',
+  styleUrl: './post-product.scss'
+})
+export class PostProduct {
+
+}

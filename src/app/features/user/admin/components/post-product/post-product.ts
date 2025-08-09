805ac83534f37ec8068049e7d01076a2b10a784d
Nestor <mateoprats.nestor@gmail.com>
2025-08-09 16:16:13 +0200

    Feat: Mak post-product standalone

--- src/app/features/user/admin/components/post-product/post-product.ts
+++ src/app/features/user/admin/components/post-product/post-product.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-product',
+  standalone: true,
   imports: [],
   templateUrl: './post-product.html',
   styleUrl: './post-product.scss'

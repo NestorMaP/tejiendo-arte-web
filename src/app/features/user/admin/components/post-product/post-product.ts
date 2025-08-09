fc44eba33aa9af3dbd16a3da09666fab3daef262
Nestor <mateoprats.nestor@gmail.com>
2025-08-09 16:32:23 +0200

    Feat[post-products.ts]: Logic to see properly the form

--- src/app/features/user/admin/components/post-product/post-product.ts
+++ src/app/features/user/admin/components/post-product/post-product.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AdminService } from '../../service/admin';
 
 @Component({
   selector: 'app-post-product',
@@ -8,5 +12,51 @@ import { Component } from '@angular/core';
   styleUrl: './post-product.scss'
 })
 export class PostProduct {
+
+  productForm: FormGroup;
+  listOfCategories: any = [];
+  selectedFile: File | null;
+  imagePreview: string | ArrayBuffer | null;
+
+  constructor(
+    private formBuilder FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private adminService: AdminService,
+  ) { }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.previewImage();
+  }
+
+  previewImage() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
+  }
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      categoryId: [null, [Validators.required]],
+      name: [null, [Validators.required]],
+      price: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+    });
+
+    this.getAllCategories();
+  }
+
+  getAllCategories() {
+    this.adminService.getAllCategories().subscribe(response => {
+      this.listOfCategories = response;
+    });
+  }
+
+  addProduct(): void {
+    
+  }
 
 }

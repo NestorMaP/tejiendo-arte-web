bd2f5ebc4848d7617c4b40d3e90d80ce52912b5b
Nestor <mateoprats.nestor@gmail.com>
2025-08-10 11:17:01 +0200

    Feat: Logic for dasboard.ts

--- src/app/features/user/admin/components/dashboard/dashboard.ts
+++ src/app/features/user/admin/components/dashboard/dashboard.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../service/admin';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
+
+  products: any[] = [];
+
+  constructor(private AdminService: AdminService) { }
+
+  ngONINit() {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.products = [];
+    this.AdminService.getAllProducts().subscribe(response => {
+      response.forEach(element => {
+        element.processedImage = 'data:image/jpeg;base64,' + element.byteImage;
+        this.products.push(element);
+      });
+    })
+  }
 
 }

5d21f1530d059304a168267fd2ff84474c030abf
Nestor <mateoprats.nestor@gmail.com>
2025-08-14 12:02:53 +0200

    Feat: Logic for CustomerService

--- src/app/features/user/customer/service/customer.ts
+++ src/app/features/user/customer/service/customer.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserStorageService } from '../../../../services/storage/user-storage.service';
+
+const BASIC_URL = "http://localhost:8080/"
 
 @Injectable({
   providedIn: 'root'
 })
 export class Customer {
+
+
+  constructor(private http: HttpClient) {}
+
+      getAllProducts(): Observable<any> {
+        return this.http.get(BASIC_URL + 'api/customer/products', {
+          headers: this.createAuthorizationHeader()
+        })
+      }
   
+      getAllProductsByName(productName): Observable<any> {
+        return this.http.get(BASIC_URL + `api/customer/search/${productName}`, {
+          headers: this.createAuthorizationHeader()
+        })
+      }
+
+      private createAuthorizationHeader(): HttpHeaders {
+        return new HttpHeaders().set(
+          'Authorization', 'Bearer ' + UserStorageService.getToken()
+        )
+      }
 }

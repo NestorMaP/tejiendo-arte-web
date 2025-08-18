16eae7482e4e944d40b94d80e30821fe040bc33c
Nestor <mateoprats.nestor@gmail.com>
2025-08-18 10:57:36 +0200

    Feat: Create new components

---
+++ src/app/features/user/admin/components/post-coupon/post-coupon.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-coupon',
+  imports: [],
+  templateUrl: './post-coupon.html',
+  styleUrl: './post-coupon.scss'
+})
+export class PostCoupon {
+
+}

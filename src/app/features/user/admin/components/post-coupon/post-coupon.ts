7b71b020928664f406b02266686e6b9889b414fd
Nestor <mateoprats.nestor@gmail.com>
2025-08-18 11:16:30 +0200

    Feat: post-coupon.ts logic

--- src/app/features/user/admin/components/post-coupon/post-coupon.ts
+++ src/app/features/user/admin/components/post-coupon/post-coupon.ts
@@ -1,12 +1,72 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AdminService } from '../../service/admin';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-post-coupon',  
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './post-coupon.html',
   styleUrl: './post-coupon.scss'
 })
 export class PostCoupon {
+  couponForm!: FormGroup;
+
+  constructor( 
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private adminService: AdminService
+  ) {}
+
+  ngOnInit() {
+    this.couponForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      code: [null, [Validators.required]],
+      discount: [null, [Validators.required]],
+      expirationDate: [null, [Validators.required]],
+    });
+  }
+
+  addCoupon() {
+    if(this.couponForm.valid) {
+      this.adminService.addCoupon(this.couponForm.value).subscribe(response => {
+        if(response.id != null) {
+          this.snackBar.open('Coupons Posted Successfully!', 'Close', {
+            duration: 5000
+          });
+          this.router.navigateByUrl('/admin/dashboard');
+        } else {
+          this.snackBar.open(response.message, 'Close', {
+            duration: 5000,
+            panelClass: 'error-snackbar'
+          });
+        }
+      });
+    } else {
+      this.couponForm.markAsTouched();
+    }
+  }
 
 }

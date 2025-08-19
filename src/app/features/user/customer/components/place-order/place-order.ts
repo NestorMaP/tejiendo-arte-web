ee4017a7497f06797f0d9b7c14d95a206e722fa2
Nestor <mateoprats.nestor@gmail.com>
2025-08-19 14:18:59 +0200

    Feat: New placeOrder component

---
+++ src/app/features/user/customer/components/place-order/place-order.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-order',
+  imports: [],
+  templateUrl: './place-order.html',
+  styleUrl: './place-order.scss'
+})
+export class PlaceOrder {
+
+}

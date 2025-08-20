bd5f7bc1d1e00e0255b2597d57463759820d3b49
Nestor <mateoprats.nestor@gmail.com>
2025-08-21 01:12:28 +0200

    Feat: Logic for post-product-faq.ts and correct a missing slash in dashboard html

--- src/app/features/user/admin/components/post-product-faq/post-product-faq.ts
+++ src/app/features/user/admin/components/post-product-faq/post-product-faq.ts
@@ -1,11 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AdminService } from '../../service/admin';
 
 @Component({
   selector: 'app-post-product-faq',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+
+  ],
   templateUrl: './post-product-faq.html',
   styleUrl: './post-product-faq.scss'
 })
 export class PostProductFaq {
 
+  productId: number;
+  FAQForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private adminService: AdminService,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.productId = Number(this.activatedRoute.snapshot.params["productId"]);
+    this.FAQForm = this.formBuilder.group({
+      question: [null, [Validators.required]],
+      answer: [null, [Validators.required]],
+    })
+  }
+
+  postFAQ() {
+    this.adminService.postFAQ(this.productId, this.FAQForm.value).subscribe(response => {
+      if (response.id != null) {
+        this.snackBar.open('FAQ Posted Successfully!', 'Close', { duration: 5000 });
+        this.router.navigateByUrl('/admin/dashboard');
+      } else {
+        this.snackBar.open('Something went wrong', 'Close', 
+          { duration: 5000, panelClass: 'error-snackbar'});
+      }
+    });
+  } 
 }

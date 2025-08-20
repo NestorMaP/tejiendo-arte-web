0db0a85b20093714237afe388bd946718062e797
Nestor <mateoprats.nestor@gmail.com>
2025-08-20 18:09:26 +0200

    Feat: Logic for my-orders.ts

--- src/app/features/user/customer/components/my-orders/my-orders.ts
+++ src/app/features/user/customer/components/my-orders/my-orders.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../../service/customer';
 
 @Component({
   selector: 'app-my-orders',
-  imports: [],
+  standalone: true,
+  imports: [
+
+  ],
   templateUrl: './my-orders.html',
   styleUrl: './my-orders.scss'
 })
 export class MyOrders {
+
+  myOrders: any;
+
+  constructor(
+    private customerService: CustomerService
+  ) { }
+
+  ngOnInit() {
+    this.getMyOrders();
+  }
+
+  getMyOrders() {
+    this.customerService.getOrdersByUserId().subscribe(response => {
+      this.myOrders = response;
+    })
+  }
 
 }

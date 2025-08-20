6b46f8528d081a2ff2558f4d6ec94397f930b4e2
Nestor <mateoprats.nestor@gmail.com>
2025-08-20 12:03:24 +0200

    Feat: Logic for adminOrders.ts

--- src/app/features/user/admin/components/orders/orders.ts
+++ src/app/features/user/admin/components/orders/orders.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../service/admin';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-orders',
+  standalone: true,
   imports: [],
   templateUrl: './orders.html',
   styleUrl: './orders.scss'
 })
 export class Orders {
+
+  orders: any;
+
+  constructor(
+    private adminService: AdminService,
+    private snackBar: MatSnackBar,
+  ) { }
+
+  ngOnInit() {
+    this.getPlacedOrders();
+  }
+
+  getPlacedOrders() {
+    this.adminService.getPlacedOrders().subscribe(response => {
+      this.orders = response;
+    });
+  }
 
 }

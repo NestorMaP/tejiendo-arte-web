08f2e119395e1868249b8b1e5a1a4afd2e0f0a5f
Nestor <mateoprats.nestor@gmail.com>
2025-08-21 18:53:33 +0200

    Feat: Generate new component

---
+++ src/app/features/user/customer/components/view-ordered-products/view-ordered-products.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-ordered-products',
+  imports: [],
+  templateUrl: './view-ordered-products.html',
+  styleUrl: './view-ordered-products.scss'
+})
+export class ViewOrderedProducts {
+
+}

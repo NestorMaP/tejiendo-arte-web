f430251892d7d145d08e08e5aca5955ed3616eec
Nestor <mateoprats.nestor@gmail.com>
2025-08-21 19:05:38 +0200

    Feat: Logic for viewing ordered products in ts

--- src/app/features/user/customer/components/view-ordered-products/view-ordered-products.ts
+++ src/app/features/user/customer/components/view-ordered-products/view-ordered-products.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CustomerService } from '../../service/customer';
 
 @Component({
   selector: 'app-view-ordered-products',
-  imports: [],
+  standalone: true,
+  imports: [
+
+  ],
   templateUrl: './view-ordered-products.html',
   styleUrl: './view-ordered-products.scss'
 })
 export class ViewOrderedProducts {
 
+  orderId: any;
+  orderedProductDetailsList = [];
+  totalAmount: any;
+
+  constructor(
+    private activatedroute: ActivatedRoute,
+    private customerService: CustomerService,
+  ) { }
+
+  ntOnInit() {
+    this.orderId = this.activatedroute.snapshot.params['orderId'];
+    this.getOrderedProductsDetailsByOrderId();
+  }
+
+  getOrderedProductsDetailsByOrderId() {
+    this.customerService.getOrderedProducts(this.orderId).subscribe(response => {
+      response.productDtoList.forEach(element => {
+        element.processedImage = 'data:image/jpeg;base64,' + element.byteImage;
+        this.orderedProductDetailsList.push(element);
+      });
+      this.totalAmount = response.orderAmount;
+    });
+  }
 }

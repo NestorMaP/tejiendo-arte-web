8008d4fb1854f7741ad1da66531cd385657651bb
Nestor <mateoprats.nestor@gmail.com>
2025-08-22 00:42:37 +0200

    Feat: Initial logic for review component.ts

--- src/app/features/user/customer/components/review-ordered-product/review-ordered-product.ts
+++ src/app/features/user/customer/components/review-ordered-product/review-ordered-product.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CustomerService } from '../../service/customer';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-review-ordered-product',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+
+  ],
   templateUrl: './review-ordered-product.html',
   styleUrl: './review-ordered-product.scss'
 })
 export class ReviewOrderedProduct {
+
+  productId: number;
+  reviewForm!: FormGroup;
+  selectedFile: File | null;
+  imagePreview: string | ArrayBuffer | null;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private customerService: CustomerService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.productId = this.activatedRoute.snapshot.params["productId"];
+    this.reviewForm = this.formBuilder.group({
+      rating: [null,[Validators.required]],
+      description: [null,[Validators.required]],
+    });
+  }
+
+  onFileSelected(event:any) {
+    this.selectedFile = event.target.files[0];
+    this.previewImage();
+  }
+
+  previewImage() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
+  }
+
+  submitForm() {
+    
+  }
 
 }

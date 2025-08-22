47aa5e567a010b48c6406664a72d61864e960281
Nestor <mateoprats.nestor@gmail.com>
2025-08-23 00:43:31 +0200

    Feat: TS Code for analytics

--- src/app/features/user/admin/components/analytics/analytics.ts
+++ src/app/features/user/admin/components/analytics/analytics.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../service/admin';
 
 @Component({
   selector: 'app-analytics',
-  imports: [],
+  standalone: true,
+  imports: [
+
+  ],
   templateUrl: './analytics.html',
   styleUrl: './analytics.scss'
 })
 export class Analytics {
 
+  data: any;
+
+  constructor(
+    private adminService: AdminService,
+  ) { }
+
+  ngOnInit() {
+    this.adminService.getAnalytics().subscribe(response => {
+      console.log(response);
+      this.data = response;
+    })
+  }
 }

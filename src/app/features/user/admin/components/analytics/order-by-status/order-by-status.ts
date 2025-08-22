575310ade47b250ff1c7f470a6a2e5f5239d84c7
Nestor <mateoprats.nestor@gmail.com>
2025-08-23 00:52:58 +0200

    TS, HTML and SCSS Code for new component, show order by status

--- src/app/features/user/admin/components/analytics/order-by-status/order-by-status.ts
+++ src/app/features/user/admin/components/analytics/order-by-status/order-by-status.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-order-by-status',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatCardModule,
+  ],
   templateUrl: './order-by-status.html',
   styleUrl: './order-by-status.scss'
 })
 export class OrderByStatus {
+
+  @Input() data:any;
 
 }

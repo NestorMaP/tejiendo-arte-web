6c320fc24099de70b1e3f82492506a924e1bfbbd
Nestor <mateoprats.nestor@gmail.com>
2025-08-23 01:03:38 +0200

    TS, HTML and SCSS Code for new component, show analyticss

--- src/app/features/user/admin/components/analytics/analytics.ts
+++ src/app/features/user/admin/components/analytics/analytics.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../service/admin';
+import { OrderByStatus } from './order-by-status/order-by-status';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
   imports: [
-
+    OrderByStatus,
+    MatCardModule,
+    
   ],
   templateUrl: './analytics.html',
   styleUrl: './analytics.scss'

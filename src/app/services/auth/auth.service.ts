a0aa6195fba5f8c5b37334f7e163c60db70db410
Nestor <mateoprats.nestor@gmail.com>
2025-08-22 23:51:26 +0200

    Feat: Correct api route

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -36,7 +36,7 @@ export class AuthService {
   }
 
   getOrderByTrackingId(trackingId: number): Observable<any> {
-    return this.http.get(BASIC_URL + `order/${trackingId}`);
+    return this.http.get(BASIC_URL + `api/order/${trackingId}`);
   }
 
 }

daa2480d0855156899e222611cf503e346ebde2c
Nestor <mateoprats.nestor@gmail.com>
2025-08-22 21:21:16 +0200

    Feat: TS for the new component

--- src/app/features/user/customer/components/view-wishlist/view-wishlist.ts
+++ src/app/features/user/customer/components/view-wishlist/view-wishlist.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../../service/customer';
 
 @Component({
   selector: 'app-view-wishlist',
-  imports: [],
+  standalone: true,
+  imports: [
+
+  ],
   templateUrl: './view-wishlist.html',
   styleUrl: './view-wishlist.scss'
 })
 export class ViewWishlist {
+
+  products: any[] = [];
+
+  constructor(
+    private customerService: CustomerService,
+  ) { }
+
+  ngOnInit() {
+    this.getWishlistByUserId();
+  }
+
+  getWishlistByUserId() {
+    this.customerService.getWishlistByUserId().subscribe(response => {
+      response.forEach(element => {
+        element.processedImage = 'data:image/jpeg;base64,' + element.byteImage;
+        this.products.push(element);
+      });
+    })
+  }
 
 }

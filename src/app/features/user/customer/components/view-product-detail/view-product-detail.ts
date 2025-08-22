f3dfa36826fbbd3336a9567be1805186064bef92
Nestor <mateoprats.nestor@gmail.com>
2025-08-22 12:27:00 +0200

    Feat: New component

---
+++ src/app/features/user/customer/components/view-product-detail/view-product-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-product-detail',
+  imports: [],
+  templateUrl: './view-product-detail.html',
+  styleUrl: './view-product-detail.scss'
+})
+export class ViewProductDetail {
+
+}

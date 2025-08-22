129cc06fe379f8ea427b0cf809a678fee9fa8c4d
Nestor <mateoprats.nestor@gmail.com>
2025-08-22 12:36:52 +0200

    Feat: TS for the new component

--- src/app/features/user/customer/components/view-product-detail/view-product-detail.ts
+++ src/app/features/user/customer/components/view-product-detail/view-product-detail.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CustomerService } from '../../service/customer';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-product-detail',
-  imports: [],
+  standalone: true,
+  imports: [
+
+  ],
   templateUrl: './view-product-detail.html',
   styleUrl: './view-product-detail.scss'
 })
 export class ViewProductDetail {
+
+  productId: number;
+
+  product: any;
+  FAQS: any[] = [];
+  reviews: any[] = [];
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private customerService: CustomerService,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.productId = this.activatedRoute.snapshot.params["productId"];
+    this.getProductDetailById();
+  }
+
+  getProductDetailById() {
+    this.customerService.getProductDetailById(this.productId).subscribe(response => {
+      this.product = response.productDto;
+      this.product.processedImagg = 'data:image/png;base64' + response.productDto.byteImage;
+
+      this.FAQS = response.faqDtoList;
+
+      response.reviewDtoList.forEach(element => {
+        element.processedImage = 'data:image/jpeg;base64,' + element.byteImage;
+        this.reviews.push(element);
+      });
+    });
+  }
 
 }

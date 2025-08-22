68a25547e955a69897f64b93fdd5eebd00a185e3
Nestor <mateoprats.nestor@gmail.com>
2025-08-22 23:28:00 +0200

    Feat: TS for new component

--- src/app/features/track-order/track-order.ts
+++ src/app/features/track-order/track-order.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-track-order',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+
+  ],
   templateUrl: './track-order.html',
   styleUrl: './track-order.scss'
 })
 export class TrackOrder {
+
+  searchOrderForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+  ) { }
+
+  ngOnInit() {
+    this.searchOrderForm = this.formBuilder.group({
+      trackingId: [null, [Validators.required]]
+    })
+  }
+
+  submitForm() {
+    this.authService.getOrderByTrackingId(this.searchOrderForm.get('trackingId').value).subscribe(response => {
+      console.log(response)
+    })
+  }
+
 
 }
